1460bd049af0adf29a4ae2d046eab6070d33a2bb
Cory <fizzy.gutsnipe@outlook.com>
2018-04-22 20:51:57 -0700

    4/22 - Cory Empty Activities and Fragments added according to Architecture Diagram

---
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/NavigationActivity.java
@@ -0,0 +1,13 @@
+package group9.tcss450.uw.edu.chatappgroup9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+    }
+}
