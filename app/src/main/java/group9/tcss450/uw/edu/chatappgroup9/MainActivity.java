a122086d1f33598705e0528ea2f240e670e52000
garrett engle <garree@gmail.com>
2018-04-23 22:25:38 -0700

    added navigation

--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
@@ -1,7 +1,10 @@
 package group9.tcss450.uw.edu.chatappgroup9;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, NavigationActivity.class);
+        EditText Name = (EditText) findViewById(R.id.Name);
+        EditText Pass = (EditText) findViewById(R.id.Password);
+        String Name1 = Name.getText().toString();
+        String Password = Pass.getText().toString();
+        if (Name1 != null && Name1.contains("@") && Password != null){
+            startActivity(intent);
+        }
     }
 }
