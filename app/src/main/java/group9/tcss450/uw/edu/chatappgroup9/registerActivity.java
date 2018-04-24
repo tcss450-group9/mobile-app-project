6fc9b2aabfa508cf767b86b9b5103df875d9af8a
garrett engle <garree@gmail.com>
2018-04-24 06:40:30 -0700

    added navigation

--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/registerActivity.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/registerActivity.java
@@ -6,28 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class registerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_register);
     }
     public void sendMessage(View view){
         Intent intent = new Intent(this, NavigationActivity.class);
-        EditText Name = (EditText) findViewById(R.id.Name);
-        EditText Pass = (EditText) findViewById(R.id.Password);
+        EditText Name = (EditText) findViewById(R.id.Name1);
+        EditText Pass = (EditText) findViewById(R.id.Password1);
         String Name1 = Name.getText().toString();
         String Password = Pass.getText().toString();
         if (Name1 != null && Name1.contains("@") && Password != null){
             startActivity(intent);
         }
     }
-    public void registerpress(View view) {
-        Intent intent =new Intent(this, registerActivity.class);
-            startActivity(intent);
 
-    }
 }
