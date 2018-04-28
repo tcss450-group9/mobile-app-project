40f47c94948aa88b567ce418fcb3dd3ac8d45b36
Minqing <chen7410@uw.edu>
2018-04-27 18:36:50 -0700

    finish login and registration client check

--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
@@ -4,37 +4,74 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String EMPTY_USERNAME = "Username cannot be empty";
-    private static final String EMPTY_PASSWORD = "Password cannot be empty";
+    private final String INVALID_LOGIN_INFO = "Invalid username or password";
+    private final int MIN_LENGTH_USERNAME_PASSWORD = 6;
+    /** Regular expression**/
+    private final Pattern REG_EX_USERNAME = Pattern.compile("[^a-zA-Z_0-9]");
+    private final Pattern REG_EX_PASSWORD =
+            Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*(_|[^\\w])).+$");
+    private EditText myUsername;
+    private EditText myPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myUsername = findViewById(R.id.mainEditTextUserName);
+        myPassword = findViewById(R.id.mainEditTextPassword);
 
 
     }
 
     public void loginOnClicked(View view){
         Intent intent = new Intent(this, NavigationActivity.class);
-        EditText name = findViewById(R.id.mainEditTextUserName);
-        EditText password = findViewById(R.id.mainEditTextPassword);
-
-        if (TextUtils.isEmpty(name.getText().toString())) {
-            name.setError(EMPTY_USERNAME);
-        }
-        if (TextUtils.isEmpty(password.getText().toString())) {
-            password.setError(EMPTY_PASSWORD);
-            return;
+        if (!isLoginInfoGood()) {
+            myUsername.setError(INVALID_LOGIN_INFO);
+            myPassword.setError(INVALID_LOGIN_INFO);
         } else {
-            //TODO login success
+            //TODO attempt to login
             startActivity(intent);
         }
+
+    }
+
+    /**
+     * @return true if the username and password is ready for the server; false otherwise.
+     */
+    private boolean isLoginInfoGood() {
+        boolean result = true;
+        String username = myUsername.getText().toString();
+        String password = myPassword.getText().toString();
+
+
+        if (TextUtils.isEmpty(username)) {
+            result = false;
+        } else if (username.length() < MIN_LENGTH_USERNAME_PASSWORD) {
+            result = false;
+        } else if (REG_EX_USERNAME.matcher(username).find()) {
+            result = false;
+            Log.e("Main", "Username contains only A-Z, a-z, 0-9 and _ :" + username);
+        }
+        else if (TextUtils.isEmpty(password)) {
+            result = false;
+        } else if (password.length() < MIN_LENGTH_USERNAME_PASSWORD) {
+            result = false;
+        } else if (!REG_EX_PASSWORD.matcher(password).matches()) {
+            result = false;
+            Log.e("Main",
+                    "Password needs at least one number " +
+                    "and one lower case letter and one " +
+                    "upper case letter and one special character: " + password);
+        }
+        return result;
     }
 
     public void toRegistrationActivity(View view) {
@@ -42,4 +79,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
     }
+
+
+
 }
--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/RegistrationActivity.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/RegistrationActivity.java
@@ -9,15 +9,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class RegistrationActivity extends AppCompatActivity {
 
-    private static int MIN_PASSWORD_LENGTH = 6;
-    private static final String USERNAME_EMPTY = "Username cannot be empty";
-    private static final String FIRST_NAME_EMPTY = "First name cannot be empty";
-    private static final String LAST_NAME_EMPTY = "Last name cannot be empty";
-    private static final String PASSWORD_NOT_MATCH = "Passwords are not match";
-    private static final String PASSWORD_TOO_SHORT = "Password less than 6 characters";
-    private  static final String EMAIL_INVALID = "Email address is invalid";
+    private int MIN_PASSWORD_LENGTH = 6;
+    private final String USERNAME_EMPTY = "Username cannot be empty";
+    private final String USERNAME_TOO_SHORT = "Username is too short";
+    private final String FIRST_NAME_EMPTY = "First name cannot be empty";
+    private final String LAST_NAME_EMPTY = "Last name cannot be empty";
+    private final String PASSWORD_NOT_MATCH = "Passwords are not match";
+    private final String PASSWORD_TOO_SHORT = "Password is too short";
+    private final String EMAIL_INVALID = "Email address is invalid";
+    private final String INVALID_CHARACTER = "Invalid character contained";
+    private final String PASSWORD_TOO_SIMPLE = "Password is too simple";
+    /** Regular expression**/
+    private final Pattern REG_EX_USERNAME = Pattern.compile("[^a-zA-Z_0-9]");
+    private final Pattern REG_EX_NAME = Pattern.compile("[^a-zA-Z]");
+    private final Pattern REG_EX_PASSWORD =
+            Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*(_|[^\\w])).+$");
+
     private EditText myUsername;
     private EditText myPassword;
     private EditText myConfirmPassword;
@@ -42,37 +53,77 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void submitOnClicked(View theSubmitButton){
-        verifyRegistrationInfo(theSubmitButton);
-        //TODO Register new account
+        if (isRegistrationInfoGood(theSubmitButton)) {
+            //TODO Register new account, and check username on server
+            Toast.makeText(getApplicationContext(),
+                    "Client check pass", Toast.LENGTH_LONG).show();
+            backToLogin();
+
+        }
+
 
 
     }
 
-    private void verifyRegistrationInfo(View theSubmitButton) {
-        if (!isValidEmail(myEmail.getText().toString())) {
+    /**
+     * @return true if the username and password is ready for the server; false otherwise.
+     */
+    private boolean isRegistrationInfoGood(View theSubmitButton) {
+        boolean result = true;
+        String email = myEmail.getText().toString();
+        String firstname = myFirstName.getText().toString();
+        String lastname = myLastName.getText().toString();
+        String username = myUsername.getText().toString();
+        String password = myPassword.getText().toString();
+        String confirmPassword = myConfirmPassword.getText().toString();
+
+        if (!isValidEmail(email)) {
+            result = false;
             myEmail.setError(EMAIL_INVALID);
         }
-        if (myFirstName.getText().toString().isEmpty()) {
-            myFirstName.setError(FIRST_NAME_EMPTY);
-        }
-        if (myLastName.getText().toString().isEmpty()) {
-            myLastName.setError(LAST_NAME_EMPTY);
-        }
-        if (myUsername.getText().toString().isEmpty()) {
-            myUsername.setError(USERNAME_EMPTY);
-        }
-        if (!myPassword.getText().toString().equals(myConfirmPassword.getText().toString())) {
-            myPassword.setError(PASSWORD_NOT_MATCH);
-            return;
-        }
-        if (myPassword.getText().toString().length() < MIN_PASSWORD_LENGTH) {
-            myPassword.setError(PASSWORD_TOO_SHORT);
-        } else {
-            Toast.makeText(getApplicationContext(),
-                    "Registration success", Toast.LENGTH_LONG).show();
-            backToLogin();
 
+        if (TextUtils.isEmpty(firstname)) {
+            result = false;
+            myFirstName.setError(FIRST_NAME_EMPTY);
+        }else if (REG_EX_NAME.matcher(firstname).find()) {
+            result = false;
+            myFirstName.setError(INVALID_CHARACTER);
         }
+
+        if (TextUtils.isEmpty(lastname)) {
+            result = false;
+            myLastName.setError(LAST_NAME_EMPTY);
+        } else if (REG_EX_NAME.matcher(lastname).find()) {
+            result = false;
+            myLastName.setError(INVALID_CHARACTER);
+        }
+
+        if (TextUtils.isEmpty(username)) {
+            result = false;
+            myUsername.setError(USERNAME_EMPTY);
+        } else if (REG_EX_USERNAME.matcher(username).find()) {
+            result = false;
+            myUsername.setError(INVALID_CHARACTER);
+        } else if (username.length() < MIN_PASSWORD_LENGTH) {
+            myUsername.setError(USERNAME_TOO_SHORT);
+        }
+
+        //password and confirm password check
+        if (!password.equals(confirmPassword)) {
+            result = false;
+            myPassword.setError(PASSWORD_NOT_MATCH);
+            myConfirmPassword.setError(PASSWORD_NOT_MATCH);
+        } else if (password.length() < MIN_PASSWORD_LENGTH) {
+            result = false;
+            myPassword.setError(PASSWORD_TOO_SHORT);
+            myConfirmPassword.setError(PASSWORD_TOO_SHORT);
+        } else if (!REG_EX_PASSWORD.matcher(password).matches()) {
+            result = false;
+            myPassword.setError(PASSWORD_TOO_SIMPLE);
+            myConfirmPassword.setError(PASSWORD_TOO_SIMPLE);
+        }
+
+        return result;
     }
 
     private void backToLogin() {
