082b32885d0a16f25813ab40d8f251209502b866
Minqing <chen7410@uw.edu>
2018-04-29 22:50:05 -0700

    add credentials and asyncTask, need to handle login failure

--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
@@ -1,14 +1,22 @@
 package group9.tcss450.uw.edu.chatappgroup9;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.regex.Pattern;
+
+import group9.tcss450.uw.edu.chatappgroup9.model.Credentials;
+import group9.tcss450.uw.edu.chatappgroup9.utils.SendPostAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,23 +35,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myUsername = findViewById(R.id.mainEditTextUserName);
         myPassword = findViewById(R.id.mainEditTextPassword);
-
+        Button login = findViewById(R.id.mainButtonLogin);
+        login.setOnClickListener(this::loginOnClicked);
 
     }
 
     public void loginOnClicked(View view){
-        Intent intent = new Intent(this, NavigationActivity.class);
         if (!isLoginInfoGood()) {
             myUsername.setError(INVALID_LOGIN_INFO);
             myPassword.setError(INVALID_LOGIN_INFO);
         } else {
             //TODO attempt to login
-            startActivity(intent);
+            Credentials loginInfo = new Credentials.Builder(myUsername.getText().toString(),
+                    myPassword.getEditableText()).build();
+
+            loginAttempt(loginInfo);
         }
 
     }
 
     /**
+     * Sends login information to login end point to verify.
+     * @param loginInfo
+     * @return true if login success; false otherwise.
+     */
+    private void loginAttempt(final Credentials loginInfo) {
+        Uri uri = new Uri.Builder()
+                .scheme("https")
+                .appendPath(getString(R.string.ep_base_url))
+                .appendPath(getString(R.string.ep_login))
+                .build();
+
+        JSONObject msg = loginInfo.asJSONObject();
+
+        //instantiate and execute the AsyncTask.
+        //Feel free to add a handler for onPreExecution so that a progress bar
+        //is displayed or maybe disable buttons. You would need a method in
+        //LoginFragment to perform this.
+        new SendPostAsyncTask.Builder(uri.toString(), msg)
+                .onPostExecute(this::handleLoginOnPost)
+//                .onCancelled(this::handleErrorsInTask)
+                .build().execute();
+    }
+
+    /**
+     * Handle onPostExecute of the AsyncTask. The result from our webservice is
+     * a JSON formatted String. Parse it for success or failure.
+     * @param result the JSON formatted String response from the web service
+     */
+    private void handleLoginOnPost(String result) {
+        try {
+            JSONObject resultsJSON = new JSONObject(result);
+            boolean success = resultsJSON.getBoolean("success");
+            //TODO need to get fail info from server.
+
+            if (success) {
+                //Login was successful. Switch to the chat page.
+                Intent intent = new Intent(this, NavigationActivity.class);
+                startActivity(intent);
+            } else {
+                //Login was unsuccessful.
+                //TODO handle failure
+                Log.e("Main activity","Log in unsuccessful");
+            }
+
+        } catch (JSONException e) {
+            //It appears that the web service didn’t return a JSON formatted String
+            //or it didn’t have what we expected in it.
+            Log.e("JSON_PARSE_ERROR",  result
+                    + System.lineSeparator()
+                    + e.getMessage());
+        }
+    }
+
+    /**
+     *
      * @return true if the username and password is ready for the server; false otherwise.
      */
     private boolean isLoginInfoGood() {
