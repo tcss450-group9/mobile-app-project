79b533f5205461c685a3881658a66eab8107bb5e
Jenzel Villanueva <villaj3@uw.edu>
2018-05-14 01:30:55 -0700

    Added Verification Activity; User is emailed a pin and verifies it. Need to prevent user from logging in if a pin still exists in db.

--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -157,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
     public void toRegistrationActivity(View view) {
         Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent);
+        this.finish();
     }
 
 
