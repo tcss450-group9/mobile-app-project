3190e9d7bdf5aaea2588033c761ff0ea9ff8363d
Minqing <chen7410@uw.edu>
2018-05-29 00:36:58 -0700

    fix forgotPassword fragment not send email bug

--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/ForgotPasswordFragment.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/ForgotPasswordFragment.java
@@ -2,7 +2,6 @@ package group9.tcss450.uw.edu.chatappgroup9;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,58 +20,16 @@ import java.util.Random;
 import group9.tcss450.uw.edu.chatappgroup9.utils.SendPostAsyncTask;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ForgotPasswordFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ForgotPasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ForgotPasswordFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private final String TAG = "ForgotPasswordFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private final String TAG = "ForgotPasswordFragment";
 
     public ForgotPasswordFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForgotPasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForgotPasswordFragment newInstance(String param1, String param2) {
-        ForgotPasswordFragment fragment = new ForgotPasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -85,6 +41,7 @@ public class ForgotPasswordFragment extends Fragment {
         b.setOnClickListener(this::onAlreadySubmit);
         return v;
     }
+
     public void onAlreadySubmit(View view){
         ResetFragment frag  = new ResetFragment();
 
@@ -94,7 +51,6 @@ public class ForgotPasswordFragment extends Fragment {
                 .commit();
         ((EditText) getView().findViewById(R.id.Forgot_password_Editext))
                 .setText("");
-
     }
 
 
@@ -108,20 +64,21 @@ public class ForgotPasswordFragment extends Fragment {
                     + " must implement OnFragmentInteractionListener");
         }
     }
+
     public void onSubmitClickForgot(View view) {
         int veri = verificationPinGenerator();
-        Log.d("gerer", "onSubmitClickForgot: here");
-        SharedPreferences prefs = getActivity().getSharedPreferences(getString(R.string.keys_shared_prefs), Context.MODE_PRIVATE);
-        String user = prefs.getString(getString(R.string.keys_shared_prefs_username), "");
-        Uri uri = new Uri.Builder().scheme("https").appendPath(getString(R.string.ep_base_url))
-                .appendPath(getString(R.string.ep_change_pass_initiate)).build();
+        Log.d(TAG, "onSubmitClickForgot: here");
+        Uri uri = new Uri.Builder().scheme("https")
+                .appendPath(getString(R.string.ep_base_url))
+                .appendPath(getString(R.string.ep_change_pass_initiate))
+                .build();
 
         //build the JSON object
         JSONObject msg = new JSONObject();
         try {
             msg.put("email",((EditText)getActivity().findViewById( R.id.Forgot_password_Editext)).getText().toString());
             msg.put("verification",veri);
-
+            Log.d(TAG, "JSON: " + msg.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -131,47 +88,31 @@ public class ForgotPasswordFragment extends Fragment {
                 .onCancelled(this::handleError)
                 .build().execute();
     }
+
     public void onBackPressed() {
         startActivity(new Intent(getContext(), MainActivity.class));
         getActivity().finish();
     }
+
     private void handleError(final String msg) {
         Log.e("CHAT ERROR!!!", msg.toString());
     }
 
-    private void handleResetOnPost(String s) {
+    private void handleResetOnPost(String result) {
         ResetFragment frag  = new ResetFragment();
-        Log.d("finished", "handleResetOnPost: finished async" + s);
+        Log.d("finished", "handleResetOnPost: finished async" + result);
         getActivity().getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragmentContainer, frag, "frag")
                 .addToBackStack(null)
                 .commit();
         ((EditText) getView().findViewById(R.id.Forgot_password_Editext))
                 .setText("");
-
-
     }
+
     public int verificationPinGenerator() {
         Random random = new Random();
-
         int value = random.nextInt(10000);
-
         return value * -1;
-    }
-
-
-    public void onClick(View view) {
-        if (mListener != null) {
-            switch (view.getId()) {
-                case R.id.Password_Reset_Submit:
-                    Log.d("here", "leaveButtonOnClick: here");
-                    onSubmitClickForgot(view);
-
-                    break;
-                default:
-                    Log.wtf("TAG", "kill me");
-            }
-        }
     }
 
     @Override
@@ -180,16 +121,7 @@ public class ForgotPasswordFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
--- app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/ResetFragment.java
+++ app/src/main/java/group9/tcss450/uw/edu/chatappgroup9/ResetFragment.java
@@ -21,14 +21,7 @@ import group9.tcss450.uw.edu.chatappgroup9.utils.InputVerificationTool;
 import group9.tcss450.uw.edu.chatappgroup9.utils.SendPostAsyncTask;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ResetFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ResetFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ResetFragment extends Fragment {
     private int MIN_LENGTH_USERNAME_PASSWORD = 6;
 
@@ -37,46 +30,11 @@ public class ResetFragment extends Fragment {
 
     private final String PASSWORD_TOO_SIMPLE = "Password is too simple";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
     public ResetFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ResetFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ResetFragment newInstance(String param1, String param2) {
-        ResetFragment fragment = new ResetFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -90,12 +48,12 @@ public class ResetFragment extends Fragment {
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
     }
+
     public void onSubmitClickForgot(View view) {
 
         Log.d("gerer", "onSubmitClickForgot: here");
@@ -109,7 +67,7 @@ public class ResetFragment extends Fragment {
         String confirmPassword = password2.getText().toString();
         Boolean result = true;
 
-        //TODO match password requirement!
+        //TODO match password requirement! fix password visible
         if (!password.equals(confirmPassword)) {
             result = false;
             password2.setError(PASSWORD_NOT_MATCH);
